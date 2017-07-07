583f7c8cd9e10f1fa138cdedfa5ecfc2381b879d
mjthomas22 <mthomas22@gmail.com>
2017-07-07 15:59:18 -0400

    Second commit

--- LuckyNumber/Program.cs
+++ LuckyNumber/Program.cs
@@ -10,50 +10,94 @@ namespace LuckyNumber
     {
         static void Main(string[] args)
         {
+            string playAgain = "";
 
+            do  
+                {
+                    Random randomNumber = new Random();
+                    int numberOfNumber = 6;
+                    int jackpot = 55000;
+                    int correctNumbers = 0;
+                    // Ask the user for a starting number for the lowest number in the number range.
+                    Console.WriteLine("Please enter the lowest possible number.");
+                    int startingNumber = int.Parse(Console.ReadLine());
 
-            // Ask the user for a starting number for the lowest number in the number range.
-            Console.WriteLine("Please enter the lowest possible number.");
-            int startingNumber = int.Parse(Console.ReadLine());
+                    // Ask the user for an ending number for the highest number in the number range.
+                    Console.WriteLine("please enter the highest possible number.");
+                    int endingNumber = int.Parse(Console.ReadLine());
 
-            // Ask the user for an ending number for the highest number in the number range.
-            Console.WriteLine("please enter the highest possible number.");
-            int endingNumber = int.Parse(Console.ReadLine());
+                    // Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+                    int[] guessNumbers = new int[numberOfNumber];
+                    for (int i = 0; i < guessNumbers.Length; i++)
+                    {
 
-            // Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
-            int[] guessNumbers = new int[6];
-            for (int i = 0; i < 6; i++)
-            {
+                        Console.WriteLine("Please enter the your numbers one at a time.");
+                        guessNumbers[i] = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Please enter the your numbers one at a time.");
-                guessNumbers[i] = int.Parse(Console.ReadLine());
-            }
-            
-            // Numbers must be stored in an array
-            // Array must be populated using a loop
-            // If the user enters a number that is outside of the range set, prompt the user to give you a valid number.Do this until the user enters a valid number.
-            //Part 2
+                        while (guessNumbers[i] < startingNumber || guessNumbers[i] > endingNumber)
+                        {
+                            Console.WriteLine("Please enter a number inbetween {0}-{1}", startingNumber, endingNumber);
+                            guessNumbers[i] = int.Parse(Console.ReadLine());
+                        }
+                    }
+                    // The program should randomly generate 6 numbers using a loop
+                    // The randomly generated numbers should be stored in an array
 
-            // The program should randomly generate 6 numbers using a loop
-            // The randomly generated numbers should be stored in an array
-            // Numbers should be displayed to the console as such and using a loop (Numbers below are for example only. Format must be exact):
-            //Lucky Number: 12
-            //Lucky Number: 47
-            //Lucky Number: 28
-            //Lucky Number: 3
-            //Lucky Number: 19
-            //Lucky Number: 35
-            //Part 3
+                    int[] luckyNumbers = new int[numberOfNumber];
 
-            // Hard - code a value for the jackpot amount and let the user know what the jackpot amount is at some point you decide in the program.
+                    for (int i = 0; i < luckyNumbers.Length; i++)
+                    {
+                        luckyNumbers[i] = randomNumber.Next(startingNumber, endingNumber + 1);
+                    }
 
-            //   The program should count the number of correctly guessed numbers and output to the console to notify the user.Example:
-            //You guessed 3 numbers correctly!
-            //   The program should calculate the user's winnings based on the number of numbers guessed correctly.
+                    Array.Sort(guessNumbers);
+                    Array.Sort(luckyNumbers);
 
-            //   The user's winnings should be output to the console. Example:
-            //  You won $256, 877.23!
-            //  Part 4
+                    for (int i = 0; i < numberOfNumber; i++)
+                    {
+                        Console.WriteLine("Lucky Number:{0}\tYour Number:{1}", luckyNumbers[i], guessNumbers[i]);
+                    }
+
+                    for (int i = 0; i < guessNumbers.Length; i++)
+                    {
+                        foreach (int number in guessNumbers)
+                            if (number == luckyNumbers[i])
+                            {
+                                correctNumbers += 1;
+                            }
+                    }
+                    Console.WriteLine("Congrats you guessed {0} numbers correctly!!!", correctNumbers);
+
+                    if (correctNumbers == 6)
+                    {
+                        Console.WriteLine("You have done the impossible you have won ${0}!!!!!!", jackpot * 1);
+                    }
+
+                    else if (correctNumbers >= 4)
+                    {
+                        Console.WriteLine("You did amazing you have won ${0}!!!", jackpot * .7);
+                    }
+
+                    else if (correctNumbers >= 2)
+                    {
+                        Console.WriteLine("You did pretty good...You have won ${0}!!", jackpot * .25);
+                    }
+
+                    else if (correctNumbers == 1)
+                    {
+                        Console.WriteLine("Eh, you got one not too bad I guess. As a consolation you have won ${0}.", jackpot * .05);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Better luck next time!!!");
+                    }
+
+                    Console.WriteLine("\n\n\nWould you like to play again? YES/NO");
+                    playAgain = Console.ReadLine();
+                }
+            while (playAgain == "yes") ;
+            Console.WriteLine("Thanks for Playing!");
+                //  Part 4
 
 
             //   Ask the user if the user would like to play again.
