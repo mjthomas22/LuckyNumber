5b4e6851a22e6440330a795a0d58a30c50f84d7d
mjthomas22 <mthomas22@gmail.com>
2017-07-07 13:44:47 -0400

    added user guesses numbers

--- LuckyNumber/Program.cs
+++ LuckyNumber/Program.cs
@@ -10,6 +10,82 @@ namespace LuckyNumber
     {
         static void Main(string[] args)
         {
+
+
+            // Ask the user for a starting number for the lowest number in the number range.
+            Console.WriteLine("Please enter the lowest possible number.");
+            int startingNumber = int.Parse(Console.ReadLine());
+
+            // Ask the user for an ending number for the highest number in the number range.
+            Console.WriteLine("please enter the highest possible number.");
+            int endingNumber = int.Parse(Console.ReadLine());
+
+            // Ask the user to guess the 6 numbers the user thinks will be the lucky numbers generated within the number range.
+            int[] guessNumbers = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+
+                Console.WriteLine("Please enter the your numbers one at a time.");
+                guessNumbers[i] = int.Parse(Console.ReadLine());
+            }
+            
+            // Numbers must be stored in an array
+            // Array must be populated using a loop
+            // If the user enters a number that is outside of the range set, prompt the user to give you a valid number.Do this until the user enters a valid number.
+            //Part 2
+
+            // The program should randomly generate 6 numbers using a loop
+            // The randomly generated numbers should be stored in an array
+            // Numbers should be displayed to the console as such and using a loop (Numbers below are for example only. Format must be exact):
+            //Lucky Number: 12
+            //Lucky Number: 47
+            //Lucky Number: 28
+            //Lucky Number: 3
+            //Lucky Number: 19
+            //Lucky Number: 35
+            //Part 3
+
+            // Hard - code a value for the jackpot amount and let the user know what the jackpot amount is at some point you decide in the program.
+
+            //   The program should count the number of correctly guessed numbers and output to the console to notify the user.Example:
+            //You guessed 3 numbers correctly!
+            //   The program should calculate the user's winnings based on the number of numbers guessed correctly.
+
+            //   The user's winnings should be output to the console. Example:
+            //  You won $256, 877.23!
+            //  Part 4
+
+
+            //   Ask the user if the user would like to play again.
+            // If the user says yes, then the program should run again from the beginning.
+            // If the user says no, then the program should say "Thanks for playing!"(Must be exact statement).
+            //Stretch Tasks:
+
+            // Make your program ensure none of the generated numbers are repeated.For example, the following is a valid output:
+            //            Lucky Number: 12
+            //Lucky Number: 47
+            //Lucky Number: 28
+            //Lucky Number: 3
+            //Lucky Number: 19
+            //Lucky Number: 35
+            //But, the following output is invalid because 12 is generated twice.
+            //Lucky Number: 12
+            //Lucky Number: 47
+            //Lucky Number: 28
+            //Lucky Number: 3
+            //Lucky Number: 19
+            //Lucky Number: 12
+            //If there is a repeated number, replace it with a new number.Do this until there are no repeated numbers.
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
